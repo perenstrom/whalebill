54f1bda5e9deff48464e5fc93a306537d9abb54d
Per Enström <enstrom.per@gmail.com>
2023-03-12 14:49:08 +0100

    Add method to generate node options hash

---
+++ helpers/generateOptionsHash.test.ts
@@ -0,0 +1,77 @@
+import { CandidateMap, Ballot, ResultNodeOptions } from 'types/types';
+import { generateOptionsHash } from './generateOptionsHash';
+
+describe('generateOptionsHash', () => {
+  const candidates: CandidateMap = new Map([
+    ['asdf', { name: 'Per', id: 'asdf' }],
+    ['qwer', { name: 'Jobjörn', id: 'qwer' }]
+  ]);
+  const savedCandidates: CandidateMap = new Map([
+    ['asdf', { name: 'Per', id: 'asdf' }],
+    ['qwer', { name: 'Jobjörn', id: 'qwer' }],
+    ['zxcv', { name: 'Charlii', id: 'zxcv' }]
+  ]);
+
+  const ballots: Ballot[] = [
+    {
+      id: 1,
+      ranking: ['asdf', 'qwer']
+    },
+    {
+      id: 2,
+      ranking: ['qwer', 'asdf']
+    },
+    {
+      id: 3,
+      ranking: ['qwer', 'asdf']
+    },
+    {
+      id: 4,
+      ranking: ['asdf', 'qwer']
+    }
+  ];
+
+  const savedBallots: Ballot[] = [
+    {
+      id: 1,
+      ranking: ['zxcv', 'asdf', 'qwer']
+    },
+    {
+      id: 2,
+      ranking: ['zxcv', 'qwer', 'asdf']
+    },
+    {
+      id: 3,
+      ranking: ['qwer', 'asdf', 'zxcv']
+    },
+    {
+      id: 4,
+      ranking: ['asdf', 'zxcv', 'qwer']
+    }
+  ];
+
+  const winners: string[] = ['tyui'];
+  const losers: string[] = ['ghjk'];
+  const positionsToFill = 5;
+
+  const options: ResultNodeOptions = {
+    ballots,
+    candidates,
+    savedBallots,
+    savedCandidates,
+    winners,
+    losers,
+    positionsToFill
+  };
+
+  it('Is a non-empty string', async () => {
+    expect(generateOptionsHash(options)).toEqual(expect.any(String));
+    expect(generateOptionsHash(options)).not.toEqual('');
+  });
+
+  it('Is idempotent', async () => {
+    expect(generateOptionsHash(options)).toEqual(generateOptionsHash(options));
+  });
+});
+
+export {};
---
+++ helpers/generateOptionsHash.ts
@@ -0,0 +1,32 @@
+import { createHash } from 'crypto';
+import { ResultNodeOptions } from 'types/types';
+
+export const generateOptionsHash = (options: ResultNodeOptions) => {
+  const {
+    ballots = [],
+    candidates = new Map(),
+    winners = [],
+    losers = [],
+    savedBallots = [],
+    savedCandidates = new Map(),
+    positionsToFill = 1
+  } = options;
+
+  const value = [
+    `b-${ballots
+      .map((ballot) => `${ballot.id}@${ballot.ranking.join('-')}`)
+      .join('-')}`,
+    `sb-${savedBallots
+      .map((ballot) => `${ballot.id}@${ballot.ranking.join('-')}`)
+      .join('-')}`,
+    `c-${[...candidates].map(([id, { name }]) => `${id}@${name}`).join('-')}`,
+    `sc-${[...savedCandidates]
+      .map(([id, { name }]) => `${id}@${name}`)
+      .join('-')}`,
+    `w-${winners.join('-')}`,
+    `l-${losers.join('-')}`,
+    `p-${positionsToFill}`
+  ].join('-');
+
+  return createHash('md5').update(value).digest('hex');
+};
