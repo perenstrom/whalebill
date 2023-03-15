db6156c3a230050ccc16dea205df78fee06a1286
Per Enström <enstrom.per@gmail.com>
2023-03-15 20:00:40 +0100

    Add outline of tree generation method

---
+++ helpers/generateTree.ts
@@ -0,0 +1,17 @@
+import { ResultNodeOptions } from 'types/types';
+
+export const generateTree = (options: ResultNodeOptions) => {
+  // Generate starting options-hash
+  // Add to queue
+  // While queue is not empty
+    // Shift array
+    // Generate node
+    // Save options-hash -> node-hash
+    // Check if node exists
+    // Calculate results
+    // Save options-hash on current node
+    // Add to queue
+  // Loop through and replace hash
+
+  return null;
+};
