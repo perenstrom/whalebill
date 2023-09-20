cd991be508dded244259fe97e7ffd74ca775d2fd
Per Enström <enstrom.per@gmail.com>
2023-09-20 18:54:42 +0200

    Update hash method

--- helpers/generateNodeHash.test.ts
+++ helpers/generateNodeHash.test.ts
@@ -33,10 +33,10 @@ describe('generateNodeHash', () => {
     const rankingHash3 = generateNodeHash(result, winners3, losers3);
     const rankingHash4 = generateNodeHash(result, winners4, losers4);
 
-    expect(rankingHash1).toEqual('w%tyui-r%asdf@5-qwer@2-zxcv@1');
-    expect(rankingHash2).toEqual('r%asdf@5-qwer@2-zxcv@1-l%tyui');
-    expect(rankingHash3).toEqual('w%tyui-r%asdf@5-qwer@2-zxcv@1-l%tyui');
-    expect(rankingHash4).toEqual('r%asdf@5-qwer@2-zxcv@1');
+    expect(rankingHash1).toEqual('w%tyui-r%asdf@5&qwer@2&zxcv@1');
+    expect(rankingHash2).toEqual('r%asdf@5&qwer@2&zxcv@1-l%tyui');
+    expect(rankingHash3).toEqual('w%tyui-r%asdf@5&qwer@2&zxcv@1-l%tyui');
+    expect(rankingHash4).toEqual('r%asdf@5&qwer@2&zxcv@1');
   });
 
   it('Returns a string uniquely identifying a list of candidates with votes, with 0s', async () => {
@@ -48,7 +48,7 @@ describe('generateNodeHash', () => {
 
     const rankingHash = generateNodeHash(result, [], []);
 
-    expect(rankingHash).toEqual('r%asdf@5-qwer@2-zxcv@0');
+    expect(rankingHash).toEqual('r%asdf@5&qwer@2&zxcv@0');
   });
 });
 
--- helpers/generateNodeHash.ts
+++ helpers/generateNodeHash.ts
@@ -3,13 +3,13 @@ export const generateNodeHash = (
   winners: string[],
   losers: string[]
 ) => {
-  const winnerPart = winners.length ? `w%${winners.join('-')}` : '';
+  const winnerPart = winners.length ? `w%${winners.join('&')}` : '';
   const convertedResults = [...results];
   const resultPart = convertedResults.length
     ? `r%${convertedResults
         .map(([candidateId, votes]) => `${candidateId}@${votes}`)
-        .join('-')}`
+        .join('&')}`
     : '';
-  const loserPart = losers.length ? `l%${losers.join('-')}` : '';
+  const loserPart = losers.length ? `l%${losers.join('&')}` : '';
   return [winnerPart, resultPart, loserPart].filter(Boolean).join('-');
 };
