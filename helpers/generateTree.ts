63782cffc554cd5f88b7eddb9cf93d6caec43e7b
Per Enström <enstrom.per@gmail.com>
2023-10-25 20:01:07 +0200

    Rework percentage calculations

--- helpers/generateTree.ts
+++ helpers/generateTree.ts
@@ -41,13 +41,7 @@ export const generateTree = (position: AdminPosition) => {
   const nodes = new Map<NodeHash, GraphNode>();
 
   // While queue is not empty
-  //let iterator = 0;
   while (queue.length > 0) {
-    //iterator += 1;
-    if (process.env.NODE_ENV === 'development') {
-      // console.log(`Iteration ${iterator}, queue length: ${queue.length}`);
-    }
-
     // Shift array
     const currentOptions = queue.shift();
     if (!currentOptions) continue;
@@ -76,7 +70,7 @@ export const generateTree = (position: AdminPosition) => {
       sortedResults: node.results,
       positionsToFill: currentOptions.options.positionsToFill,
       previousWinners: node.winners,
-      previousLosers: node.losers,
+      previousLosers: node.losers
     });
 
     // Save options-hash on current node
@@ -86,36 +80,99 @@ export const generateTree = (position: AdminPosition) => {
     queue.push(...childOptions);
   }
 
-  // Loop through and
-  // 1. replace options-hashes with node-hashes
-  // 2. calculate percentages
+  // Generate edges and map of connections in both directions
+  // Also replace options-hash in children with node-hash
+  const edges: Edge<{}>[] = [];
+  const nodeConnections = new Map<
+    string,
+    { parents: string[]; children: string[] }
+  >();
+
+  nodes.forEach((node) => {
+    let nodeConnectionsEntry = nodeConnections.get(node.hash);
+    if (!nodeConnectionsEntry) {
+      const newConnectionEntry = { parents: [], children: [] };
+      nodeConnections.set(node.hash, newConnectionEntry);
+      nodeConnectionsEntry = newConnectionEntry;
+    }
+
+    node.children = node.children.map((childOptionsHash, i) => {
+      const resultHash = hashMap.get(childOptionsHash);
+      if (!resultHash) throw new Error('Invalid options hash');
+
+      edges.push({
+        id: `${node.hash}-${i}`,
+        source: node.hash,
+        target: resultHash
+      });
+
+      let childNodeConnectionsEntry = nodeConnections.get(resultHash);
+      if (!childNodeConnectionsEntry) {
+        const newChildConnectionEntry = { parents: [], children: [] };
+        nodeConnections.set(resultHash, newChildConnectionEntry);
+        childNodeConnectionsEntry = newChildConnectionEntry;
+      }
+
+      childNodeConnectionsEntry.parents.push(node.hash);
+
+      return resultHash;
+    });
+
+    nodeConnectionsEntry.children = node.children;
+  });
+
+  // Calculate percentages
   const nodePercentageMap = new Map<string, number>();
   const firstResultHash = (nodes.entries().next().value[1] as GraphNode).hash;
   if (!firstResultHash) throw new Error('Invalid options hash');
 
-  nodePercentageMap.set(firstResultHash, 100);
+  const percentageCalculationQueue = [firstResultHash];
 
-  nodes.forEach((node) => {
-    const newChildren: string[] = [];
-    node.children.forEach((childOptionsHash) => {
-      const resultHash = hashMap.get(childOptionsHash);
-      if (!resultHash) throw new Error('Invalid options hash');
+  while (percentageCalculationQueue.length > 0) {
+    const currentNodeHash = percentageCalculationQueue.shift();
+    if (!currentNodeHash) throw new Error('Queue empty');
 
-      newChildren.push(resultHash);
+    // Node is already done
+    if (nodePercentageMap.has(currentNodeHash)) continue;
 
-      const percentage = nodePercentageMap.get(node.hash) ?? 100;
-      const childPercentage = nodePercentageMap.get(resultHash) ?? 0;
-      nodePercentageMap.set(
-        resultHash,
-        childPercentage + percentage / node.children.length
+    const currentNodeConnections = nodeConnections.get(currentNodeHash);
+    if (!currentNodeConnections) throw new Error('Invalid node hash');
+
+    // Check if node has no parents
+    if (currentNodeConnections.parents.length === 0) {
+      nodePercentageMap.set(currentNodeHash, 100);
+      percentageCalculationQueue.push(...currentNodeConnections.children);
+      continue;
+    }
+
+    // Check if all parents have percentages calculated
+    if (
+      currentNodeConnections.parents.every((parentHash) =>
+        nodePercentageMap.has(parentHash)
+      )
+    ) {
+      // Percentage is sum of (parent percentage / number of parent children)
+      const nodePercentage = currentNodeConnections.parents.reduce(
+        (acc, parentHash) => {
+          const numberOfSiblings =
+            nodeConnections.get(parentHash)?.children.length ?? 1;
+          const parentPercentage = nodePercentageMap.get(parentHash) ?? 100;
+          const inheritedPercentage = parentPercentage / numberOfSiblings;
+
+          return acc + inheritedPercentage;
+        },
+        0
       );
-    });
 
-    node.children = newChildren;
-  });
+      nodePercentageMap.set(currentNodeHash, nodePercentage);
+      percentageCalculationQueue.push(...currentNodeConnections.children);
+    } else {
+      // Otherwise move to end of queue
+      percentageCalculationQueue.push(currentNodeHash);
+    }
+  }
 
   const newNodes: Node<GraphNodeData | OverflowData>[] = [];
-  const edges: Edge<{}>[] = [];
   nodes.forEach((node) => {
     newNodes.push({
       id: node.hash,
@@ -129,14 +186,6 @@ export const generateTree = (position: AdminPosition) => {
         }
       },
       type: NODE_TYPE_GRAPH_NODE
-    });
-
-    node.children.forEach((child, i) => {
-      edges.push({
-        id: `${node.hash}-${i}`,
-        source: node.hash,
-        target: child
-      });
     });
   });
 
